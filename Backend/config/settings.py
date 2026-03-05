684c6173bcec48c75711b3ae1134d9fbc6c5160f
Chinmay Gawande <gawandechinu@gmail.com>
2026-03-06 00:10:13 +0530

    Fix allowed host

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='task-management-system-production-88ad.up.railway.app').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
